c9658a420f39b42267a31a123273e7d4bb05357a
Esteban <esteban.duque1@outlook.com>
2025-02-26 18:46:58 -0500

    fix

---
+++ pages/Temperatura.py
@@ -0,0 +1,5 @@
+import pandas as pd
+import utilidades as util
+import streamlit as st
+
+util.generarMenu()
--- utilidades.py
+++ utilidades.py
@@ -9,7 +9,7 @@ def generarMenu():
     with st.sidebar:
         col1, col2 = st.columns(2)
         with col1:
-            image = Image.open("media\icono_pag2.png")
+            image = Image.open("media/imagen_2.jpeg")
             st.image(image, use_container_width=False)
         with col2:
             st.header("SMEC")
@@ -17,37 +17,3 @@ def generarMenu():
         st.page_link('app.py', label="Inicio")
         st.page_link('pages/pronostico.py', label='Pronostico')
     
-#Funcion del modelo predictivo
-'''
-
-def modelo_rf(df_p):
-    st.markdown('## Datos Enfermedades de Pacientes')
-    st.write(df_p.head())
-    st.subheader('Resultado del modelo Ramdom Forest')
-    #Variable a predecir
-    y = df_p.iloc[:,0]
-    #Variables predictoras
-    x = df_p.iloc[:,1:]
-    #Variables de prueba -> prueba
-    #Variables de entrenamiento -> entrenar
-    x_entrenar, x_prueba, y_entrenar, y_prueba = train_test_split(x, y,train_size=0.8, random_state=42)
-
-    st.markdown('### Separamos los datos')
-    st.write('Datos de entrenamiento')
-    st.info(f'Muestra de variables de entrenamiento: {x_entrenar.shape[0]} datos')
-    st.info(f'Muestra de variables de prueba: {x_entrenar.shape[1]} datos')
-
-    #CReamos el bosque
-
-    bosque = RandomForestClassifier()
-
-    #Entrenar el bosque
-
-    bosque.fit(x_entrenar, y_entrenar)
-
-    #Hacemos la prediccion
-
-    y_prediccion = bosque.predict(x_prueba)
-    accuracy = accuracy_score(y_prueba, y_prediccion)
-    st.write('Metrica de precisión de puntos obtenidos')
-    st.info(accuracy)'''
