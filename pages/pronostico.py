955e8a54a30aaf5da7d0feb56edcdc8ab638f38c
Esteban <esteban.duque1@outlook.com>
2025-02-27 22:45:00 -0500

    se eliminan imagenes

--- pages/pronostico.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import utilidades as util
-import streamlit as st
-import seaborn as sns
-
-util.generarMenu()
-
-st.title("Sindrome Metabolico de Enfernmedad Cardiovascular")
-
